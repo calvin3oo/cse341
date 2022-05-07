75da1cf8bd5cd0b3404193acd5a42ed2e36e257d
calvin3oo <86371905+calvin3oo@users.noreply.github.com>
2022-05-07 17:09:25 -0600

    added crud operations

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,7 +3,30 @@ const router = express.Router();
 const mongo = require('../controllers/mongo.js');
 const  ObjectId = require('mongodb').ObjectId;
 
-router.use('/allDocuments', async (req, res) => {
+const entrys = [
+    { 
+        name:"firstName", 
+        required: true
+    },
+    { 
+        name:"lastName", 
+        required: true
+    },
+    { 
+        name:"birthday", 
+        required: true
+    },
+    { 
+        name:"email", 
+        required: true
+    },
+    { 
+        name:"favoriteColor", 
+        required: true
+    }
+];
+
+router.get('/allDocuments', async (req, res) => {
     const client = await mongo.connectToMongoDB();
     const col = client.db("cse341").collection("contacts");
 
@@ -14,7 +37,7 @@ router.use('/allDocuments', async (req, res) => {
 });
 
 //format: ?id=xxxxxx
-router.use('/findDocument', async (req, res) => {
+router.get('/findDocument', async (req, res) => {
     const id = ObjectId(req.query.id);
     const client = await mongo.connectToMongoDB();
 
@@ -22,6 +45,67 @@ router.use('/findDocument', async (req, res) => {
 
     const find = await col.findOne({_id: id});
     res.send(find);
+    client.close();
+});
+
+
+/**
+ * everything in the body is required.
+ * inserts a new document into the "cse341" database, in the "contacts" collection
+ * 
+ * body format:
+ * { 
+ *  firstName: "name",
+ *  lastName: "last",
+ *  email: "email",
+ *  favoriteColor: "color",
+ *  birthday: "birthday"
+ * }
+ */
+router.post('/new', async (req, res) => {
+    const client = await mongo.connectToMongoDB();
+    const col = client.db("cse341").collection("contacts");
+    var newDoc = {};
+
+    entrys.forEach(entry => {
+        if(!req.body[entry.name] && entry.required) throw new Error(`Missing Entry: ${entry.name}`);
+        newDoc[entry.name] = req.body[entry.name];
+    });
+
+    const result = await col.insertOne(newDoc);
+
+    res.send(result.insertedId);
+    client.close();
+});
+
+//update a document with the ID provided
+//the body contains the updates to the document
+router.put('/:docID', async (req, res) => {
+    const client = await mongo.connectToMongoDB();
+    const col = client.db("cse341").collection("contacts");
+
+    const filter = { _id : ObjectId(req.params.docID)};
+    const update = {
+        $set: req.body
+    };
+
+    const response = await col.updateOne(filter, update);
+
+    res.sendStatus(200);
+    client.close();
+});
+
+//update a document with the ID provided
+//the body contains the updates to the document
+router.delete('/:docID', async (req, res) => {
+    const client = await mongo.connectToMongoDB();
+    const col = client.db("cse341").collection("contacts");
+
+    const filter = {_id : ObjectId(req.params.docID)};
+
+    const response = await col.deleteOne(filter);
+
+    res.sendStatus(200);
     client.close();
 });
 
--- server.js
+++ server.js
@@ -4,17 +4,22 @@ require('dotenv').config()
 // express framework
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 
 //mongo connection
 // const mongo = require('./controllers/mongo.js');
 // mongo.connectToMongoDB().catch(console.error);
 
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //using ejs
 app.use(express.static(__dirname + '/static'));  //static files
 
 //http://expressjs.com/en/starter/static-files.html
 app.use(express.static('.'));
+
+//make request bodies into json 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Listen for requests
 app.set("port", process.env.PORT ); 
